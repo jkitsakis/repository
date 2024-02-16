b8ba549f92d112c8f8e5d9a44745bc0fbd4906ef
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2024-02-16 13:38:34 +0200

    upd

--- pelican-website-euipo/pelicanconf.py
+++ pelican-website-euipo/pelicanconf.py
@@ -1,50 +1,38 @@
 from __future__ import unicode_literals
 
-import os
-ENV = os.environ
-
-#AUTHOR = 'jk'
-SITENAME = 'EUIPO dev'
-
 LOCAL_BUILD = True
 SITEURL = '' if LOCAL_BUILD else '/pelican/euipo'
 
+SITENAME = 'EUIPO dev'
 PATH = "content"
-
 TIMEZONE = 'Europe/Athens'
 DEFAULT_LANG = 'en'
 THEME = './theme/notmyidea'
-
 CACHE_CONTENT = False
 DELETE_OUTPUT_DIRECTORY = True
 ARTICLE_ORDER_BY = 'order'
-
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
 DEFAULT_PAGINATION = 10
-
 PLUGIN_PATHS = ['C:\Workspace\My-Applications\GitHub\pelican-plugins']
 PLUGINS = [
-    # ...
     "jinja2content",
 ]
+
+# Blogroll
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
+#
+# # Social widget
+# SOCIAL = (
+#     ("You can add links in your config file", "#"),
+#     ("Another social link", "#"),
+# )
