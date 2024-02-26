4a27b5c89035e411bbf92693fd05597fa9cedf67
yannis.kitsakis <j_kitsakis@yahoo.com>
2024-02-26 19:20:13 +0200

    cv

--- Docker-sage-ssh-latex/pandoc_files/run.sh
+++ Docker-sage-ssh-latex/pandoc_files/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+pandoc -s header.md CV.md -f markdown -t pdf --pdf-engine=wkhtmltopdf --pdf-engine-opt=--enable-local-file-access -c resume-stylesheet.css  -o resume.pdf
 
-pandoc CV.md -f markdown -t pdf --pdf-engine=wkhtmltopdf --pdf-engine-opt=--enable-local-file-access -c resume-stylesheet.css -s -o resume.pdf
+#pandoc CV.md --template=template.tex --pdf-engine=xelatex -o index.pdf
