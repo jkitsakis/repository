5f7ad698b808abb14a20995c5241c815a9865394
jkitsakis <j_kitsakis@yahoo.com>
2024-03-13 15:58:29 +0200

    euipo use env var for pelican plugins

--- pelican-website-euipo/pelicanconf.py
+++ pelican-website-euipo/pelicanconf.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import os
 
 LOCAL_BUILD = True
 SITEURL = '' if LOCAL_BUILD else '/pelican/euipo'
@@ -11,28 +12,13 @@ THEME = './theme/notmyidea'
 CACHE_CONTENT = False
 DELETE_OUTPUT_DIRECTORY = True
 ARTICLE_ORDER_BY = 'order'
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 DEFAULT_PAGINATION = 5
-PLUGIN_PATHS = ['C:\Workspace\My-Applications\GitHub\pelican-plugins']
+PLUGIN_PATHS = [os.getenv('PYTHON_PLUGINS_PATH')]
 PLUGINS = [
     "jinja2content",
 ]
-
-# Blogroll
-# LINKS = (
-#     ("Pelican", "https://getpelican.com/"),
-#     ("Python.org", "https://www.python.org/"),
-#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-#     ("You can modify those links in your config file", "#"),
-# )
-#
-# # Social widget
-# SOCIAL = (
-#     ("You can add links in your config file", "#"),
-#     ("Another social link", "#"),
-# )
