4262cd012a75e8f643014ec34013a0f3a59dcce8
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2024-11-15 16:09:40 +0200

    new seats

---
+++ planner/book-desk/AMALIASMesazzine-005.bat
@@ -0,0 +1,15 @@
+@echo off 
+
+set data="{\"dates\":[\"%1\"],\"facilityId\":\"6448cee7bf530c0226ea4d4d\",\"positionId\":\"6448cee7bf530c0226ea4d52\",\"x\":1111,\"y\":1530,\"code\":\"005\",\"parking\":[{\"parkingRequested\":false,\"plateNumber\":\"\",\"type\":\"All\"}],\"datesAndType\":[{\"date\":\"%1\",\"type\":\"0036\",\"clientName\":\"\",\"clientAddress\":\"\"}]}"
+
+:start
+title %1
+for /f "tokens=*" %%i in ('curl -o NUL -s -w "%%{http_code}" -X POST %url% -H %header% -H %cookie% -d %data%') do set HTTP_STATUS=%%i
+
+if %HTTP_STATUS% == 200 (
+	echo OK ... 
+    pause
+) else (
+	echo %HTTP_STATUS%	
+    goto start
+)
