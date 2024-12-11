16f5d268509ddadd56958007d7d2acbd579df69f
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2024-12-11 17:03:41 +0200

    planner-app

--- planner/availability-and-booking/run-availability.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:start
-title Planner Availability Check
-echo Running Python script...
-python planner-available.py
-echo Planner Availability script finished.
-pause
--- planner/availability-and-booking/run-book.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:start
-title Planner Book Desk
-echo Running Python script...
-python planner-book.py
-pause
