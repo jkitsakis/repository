178d8255b7c82ea8ddf255dc99ee97afcb081c0e
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2025-01-17 15:02:35 +0200

    configuration updates

--- pelican-website-doctor/pelicanconf.py
+++ pelican-website-doctor/pelicanconf.py
@@ -8,12 +8,49 @@ SITESUBTITLE = Parameters.SITESUBTITLE
 SITESUBTITLE_ESY = Parameters.SITESUBTITLE_ESY
 HOME = Parameters.HOME
 SERVICES = Parameters.SERVICES
-
+KEYWORDS = Parameters.KEYWORDS
+SITE_DESCRIPTION = Parameters.SITE_DESCRIPTION
 
 PATH = "content"
-TIMEZONE = 'Europe/Rome'
+TIMEZONE = 'Europe/Athens'
 DEFAULT_LANG = 'Greek'
 DEFAULT_DATE = 'fs' #Pelican will use the file system's last modified date
+
+THEME = 'theme/attila'
+HOME_COVER = 'https://img.freepik.com/premium-photo/doctor-front-bright-background-with-stethoscope-hand_397897-55.jpg?w=1380'
+INDEX_SAVE_AS = 'index.html'  # Ensure the homepage is saved as index.html
+ARTICLE_ORDER_BY = 'order'
+PAGE_ORDER_BY = 'order'
+DEFAULT_PAGINATION = 10
+
+
+MENUITEMS = [
+    (HOME, '/'),
+    (SERVICES, '/services/services.html')
+]
+
+USE_TAGS = False
+TAGS_URL = ''
+TAGS_SAVE_AS = ''
+ARCHIVES_URL = ''
+ARCHIVES_SAVE_AS = ''
+AUTHOR_URL = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_URL = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_URL = ''
+CATEGORIES_SAVE_AS = ''
+
+# URL structure for categories
+CATEGORY_URL = 'services/{slug}.html'  # Single category URL
+CATEGORY_SAVE_AS = 'services/{slug}.html'  # Save path for single category pages
+
+# URL structure for individual articles
+ARTICLE_URL = 'services/{category}/{slug}.html'
+ARTICLE_SAVE_AS = 'services/{category}/{slug}.html'
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['plugins.inject_constants', 'plugins.article_order_navigation']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,53 +70,3 @@ SOCIAL = (
     ("You can add links in your config file", "#"),
     ("Another social link", "#"),
 )
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
-#---------------------------------------------------#
-
-
-THEME = 'theme/attila'
-HOME_COVER = 'https://img.freepik.com/premium-photo/doctor-front-bright-background-with-stethoscope-hand_397897-55.jpg?w=1380'
-INDEX_SAVE_AS = 'index.html'  # Ensure the homepage is saved as index.html
-
-
-
-MENUITEMS = [
-    (HOME, '/'),
-    (SERVICES, '/services/services.html')
-]
-DISPLAY_CATEGORIES_ON_MENU = True
-DISPLAY_PAGES_ON_MENU = True
-ARTICLE_ORDER_BY = 'order'
-PAGE_ORDER_BY = 'order'
-
-
-USE_TAGS = False
-TAGS_URL = ''
-TAG_SAVE_AS = ''
-
-# URL structure for categories
-CATEGORY_URL = 'services/{slug}.html'  # Single category URL
-CATEGORY_SAVE_AS = 'services/{slug}.html'  # Save path for single category pages
-CATEGORIES_URL = ''
-# CATEGORIES_SAVE_AS = 'services.html'  # Save path for the categories index page
-
-# URL structure for individual articles
-ARTICLE_URL = 'services/{category}/{slug}.html'
-ARTICLE_SAVE_AS = 'services/{category}/{slug}.html'
-
-PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['plugins.inject_constants', 'plugins.update_category', 'plugins.article_order_navigation']
-
-
-# Translations
-DEFAULT_LANG = 'el'
-I18N_SUBSITES = {
-    'en': {
-        'SITENAME': 'KONSTANTINIDOY KYRIAKI',
-        'SITESUBTITLE': 'General Practitian',
-        'LOCALE': 'en_US'
-    }
-}
