d51133994e211409440458619f13245b263f588d
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2025-02-14 12:12:37 +0200

    update bat files to build

--- pelican-website-euipo/run.bat
+++ pelican-website-euipo/run.bat
@@ -5,7 +5,7 @@ SET DEMO_NAME=EUIPO - Pelican
 SET APP_HOME=%cd%
 SET PYTHON_ENV=%APP_HOME%\.venv
 SET PYTHON_SCRIPTS_PATH=%PYTHON_ENV%\Scripts
-SET PATH=%PYTHON_SCRIPTS_PATH%
+SET PATH=%PYTHON_SCRIPTS_PATH%;%PATH%
 
 :start
 cls   
@@ -19,7 +19,6 @@ echo   2. Run %DEMO_NAME%
 echo   3. Export requirements.txt %DEMO_NAME%
 echo   4. Upgrade packages %DEMO_NAME%
 
-call %PYTHON_SCRIPTS_PATH%\activate
 
 set /p opt=Type option:
 if "%opt%"=="0" goto exit
@@ -32,52 +31,55 @@ goto start
 
 
 :cmd
+call %PYTHON_SCRIPTS_PATH%\activate
 start cmd /k "echo Command prompt started with the environment & cd /d %PYTHON_SCRIPTS_PATH%"
 pause
 goto start
 
 :req
-cd %PYTHON_SCRIPTS_PATH%
+call %PYTHON_SCRIPTS_PATH%\activate
+cd %APP_HOME%
 call pip freeze > requirements.txt
-move requirements.txt %APP_HOME%
+move requirements.txt %APP_HOME%  2>nul
 pause
 goto start
 
 :update-packages
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
+call python -m pip install --upgrade pip  || echo ❌ Failed to upgrade pip!
 call pip install --upgrade --no-cache-dir -r requirements.txt
 pause
 goto start
 
-
 :build
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
 rmdir /s /q output
 call pelican content --delete-output
 call pelican content -D
 pause
-goto start 
+goto start
 
 :run
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
 start cmd /k call pelican -lr -p %PORT%
 pause
-REM call "C:\Users\ikitsakis\AppData\Local\Programs\Opera\opera.exe"  --ran-launcher --remote http://localhost:%PORT%
 start chrome --new-window http://localhost:%PORT%
 pause
-goto start 
+goto start
 
 :debug
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
-call pelican content --debug 
-REM > Debug.txt 2>&1
+call pelican content --debug
 pause
-goto start 
-
+goto start
 
 :exit
-cd %PYTHON_SCRIPTS_PATH%
+call %PYTHON_SCRIPTS_PATH%\activate
+call deactivate
 cls
 exit
-
 pause
--- pelican-website/run.bat
+++ pelican-website/run.bat
@@ -5,7 +5,7 @@ SET DEMO_NAME=SandyCV
 SET APP_HOME=%cd%
 SET PYTHON_ENV=%APP_HOME%\.venv
 SET PYTHON_SCRIPTS_PATH=%PYTHON_ENV%\Scripts
-SET PATH=%PYTHON_SCRIPTS_PATH%
+SET PATH=%PYTHON_SCRIPTS_PATH%;%PATH%
 SET OIKOGENEIAKOS_IATROS_DOMAIN=C:\Workspace\My-Applications\GitHub\oikogeneiakos-iatros-alimos.github.io
 
 :start
@@ -21,8 +21,6 @@ echo   3. Export requirements.txt %DEMO_NAME%
 echo   4. Upgrade packages %DEMO_NAME%
 echo   5. Deploy %DEMO_NAME%
 
-call %PYTHON_SCRIPTS_PATH%\activate
-
 set /p opt=Type option:
 
 if "%opt%"=="0" goto exit
@@ -36,52 +34,55 @@ goto start
 
 
 :cmd
+call %PYTHON_SCRIPTS_PATH%\activate
 start cmd /k "echo Command prompt started with the environment & cd /d %PYTHON_SCRIPTS_PATH%"
 pause
 goto start
 
 :req
-cd %PYTHON_SCRIPTS_PATH%
+call %PYTHON_SCRIPTS_PATH%\activate
+cd %APP_HOME%
 call pip freeze > requirements.txt
-move requirements.txt %APP_HOME%
+move requirements.txt %APP_HOME%  2>nul
 pause
 goto start
 
 :update-packages
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
+call python -m pip install --upgrade pip  || echo ❌ Failed to upgrade pip!
 call pip install --upgrade --no-cache-dir -r requirements.txt
 pause
 goto start
 
-
 :build
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
 rmdir /s /q output
 call pelican content --delete-output
 call pelican content -D
 pause
-goto start 
+goto start
 
 :run
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
 start cmd /k call pelican -lr -p %PORT%
 pause
-REM call "C:\Users\ikitsakis\AppData\Local\Programs\Opera\opera.exe"  --ran-launcher --remote http://localhost:%PORT%
 start chrome --new-window http://localhost:%PORT%
 pause
-goto start 
+goto start
 
 :debug
+call %PYTHON_SCRIPTS_PATH%\activate
 cd %APP_HOME%
-call pelican content --debug 
-REM > Debug.txt 2>&1
+call pelican content --debug
 pause
-goto start 
-
+goto start
 
 :exit
-cd %PYTHON_SCRIPTS_PATH%
+call %PYTHON_SCRIPTS_PATH%\activate
+call deactivate
 cls
 exit
-
 pause
