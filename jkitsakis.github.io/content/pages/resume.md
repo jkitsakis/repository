17cf884f648d5f06fd91ee3f4ee6cde92ae6b2e0
KITSAKIS Ioannis <yannis.kitsakis@intrasoft-intl.com>
2025-02-14 15:09:52 +0200

    updqate pelican conf

---
+++ jkitsakis.github.io/content/pages/resume.md
@@ -0,0 +1,61 @@
+Title: John Kitsakis Resume
+Date: 2025-02-14
+Category: Resume
+Tags: software engineer, Java, Spring, HTML5, CSS, JEE, Pelican, technical skills
+
+# John Kitsakis - Software Engineer Resume
+
+## Career Summary
+
+Experienced Software Engineer with a demonstrated history of working in the information technology and services industry. Solid professional experience and excellent technical skills mostly with JEE standards. I have worked for several EU and public sector projects, implementing and delivering high-quality software. At Intrasoft International, I have participated in numerous projects delivering e-Customs solutions for many countries like Greece, UK, Malaysia, Singapore, Thailand, FYROM, and others. In recent years, I worked in the e-payments services sector, specifically with the core Credit Card Issuing Process application of Wirecard AG, as well as in the e-Wallet application for Wirecard NZ Ltd. I had the opportunity to lead a team of four developers, QA, and BA according to Scrum (Agile) methodologies.
+
+## Skills
+
+- **JAVA**: 90%  
+- **Spring Framework**: 95%  
+- **HTML5 & CSS**: 95%  
+- **Hibernate**: 90%  
+- **JPA**: 85%  
+
+## Languages
+
+- **Greek**: Native
+- **English**: Professional
+
+## Interests
+
+- Coding
+- Music
+
+## Experience
+
+### Software Development Engineer | Netcompany-Intrasoft
+**Sep 2020 - Present**  
+Part of the web team working on the European Union Intellectual Property Office (EUIPO).
+
+### Software Development Engineer | Wirecard
+**Mar 2019 - Aug 2020**  
+Worked on developing E-Payment System at the Issuing Department of Wirecard.
+
+### Software Engineer | Intrasoft International
+**May 2016 - Aug 2016**  
+Worked on the Greek Customs Transit module (NCTS), Transit System for Skopje Customs (CDEPS), and e-Customs Services for ASIAN Customs.
+
+## Education
+
+### MSc, Computer Science | University of Wales - Swansea
+**2001 - 2002**
+
+### Bachelor of Science (BSc), Computer Science | Hellenic Open University
+**2000**
+
+## Certifications
+
+### Data Science with R Language - Credential ID 92130 | University of the Aegean
+**2022**
+
+### Python Programming - Credential ID 92262 | University of the Aegean
+**2022**
+
+### Java Platform Enterprise Edition 7 (Java EE 7) - Credential ID 8765675 | Java Certification
+**2014**
--- jkitsakis.github.io/pelicanconf.py
+++ jkitsakis.github.io/pelicanconf.py
@@ -1,21 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+# pelicanconf.py
 
-AUTHOR = 'Yannis Kitsakis'
-SITENAME = 'Yannis Kitsakis - Website'
+AUTHOR = 'John Kitsakis'
+SITENAME = 'John Kitsakis - Resume'
 SITEURL = ''
-THEME_STATIC_DIR='theme/css/'
-CSS_FILE='main-2.css'
+PATH = 'content'
 TIMEZONE = 'Europe/Athens'
 DEFAULT_LANG = 'en'
-THEME = './themes/resume'
-INDEX = './content/pages/about.md'
-INDEX_SAVE_AS = 'index.html'
-PATH = 'content'
-CACHE_CONTENT='false'
-DELETE_OUTPUT_DIRECTORY = True
+THEME = 'themes/resume'  # Or your chosen theme
+USE_TAGS = False
+TAGS_URL = ''
+TAGS_SAVE_AS = ''
+ARCHIVES_URL = ''
+ARCHIVES_SAVE_AS = ''
+AUTHOR_URL = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_URL = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_URL = ''
+CATEGORIES_SAVE_AS = ''
+CATEGORY_URL = ''  # Single category URL
+CATEGORY_SAVE_AS = ''  # Save path for single category pages
+ARTICLE_URL = ''
+ARTICLE_SAVE_AS = ''
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['pelican_resume']  # Add this plugin if using pelican-resume
+
+# Optional: Add your resume content markdown file path
+RESUME_SRC = 'pages\\resume.md'
+RESUME_PDF = 'pdfs\\resume.pdf'
+RESUME_TYPE = 'moderncv'  # or 'moderncv'
 #Profile information
 NAME = 'Kitsakis Yannis'
 TAGLINE = 'Full Stack Developer'
@@ -25,24 +44,8 @@ FAVICON = 'favicon.ico'
 EMAIL = 'j_kitsakisk@yahoo.com'
 LINKEDIN = 'ikitsakis'
 GITHUB = 'jkitsakis'
-#PHONE = '(+91) 9560038966'
-#WEBSITE = 'suheb.in'
-#TWITTER = '@iamsuheb'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = True
-
-
 
 CAREER_SUMMARY = 'Exprerienced Software Engineer with a demonstrated history of working in the information technology and services industry. Solid professional experience and excellent technical skills mostly with JEE standards. I have worked for a number EU and Public sector projects, implementing and delivering high quality software. Working in Intrasoft Intl, I have participated in many projects delivering e-Customs solutions for many counties like Greece, UK, Malaysia, Singapore, Thailand, FYROM, and others. Ιn previous year and beyond, I worked in e-payments services. Specifically I worked in main/core Credit Card Issuing Process application of Wirecard AG, as long as in e-Wallet application for Wirecard NZ Ltd. I had the opportunity to lead a team of 4 developers QA and BA according to Scrum (Agile) methodologies.'
-
 
 SKILLS = [
 	{
